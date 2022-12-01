0437d7cdc95bee027a33388950d764b9818e035c
Magnus Pettersson <magnus@entropy.nu>
2022-12-01 21:25:04 +0100

    updated compile, added extract

--- compile/entry.sh
+++ compile/entry.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
 
+cd /opt
 
-cmake .. -DCMAKE_INSTALL_PREFIX=/opt/server -DBUILD_EXTRACTORS=ON -DPCH=1 -DDEBUG=0 -DBUILD_PLAYERBOT=ON
+if [ $DOWNLOAD_SOURCE = 'ON' ]; then
+    git clone https://github.com/cmangos/mangos-tbc
+    git clone https://github.com/cmangos/tbc-db
+fi
+
+if [ $UPDATE_SOURCE = 'ON' ]; then
+   cd /opt/mangos-tbc
+   git pull
+
+   cd /opt/tbc-db
+   git pull
+fi
+
+mkdir -p /opt/mangos-tbc/build
+cd /opt/mangos-tbc/build
+
+if [ $CREATE_DIRS = 'ON' ]; then
+    mkdir -p $INSTALL_PREFIX/logs
+    mkdir -p $INSTALL_PREFIX/data
+fi
+
+cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DBUILD_EXTRACTORS=$EXTRACTORS -DPCH=$PCH -DDEBUG=$DEBUG -DBUILD_PLAYERBOT=$PLAYERBOT -DBUILD_AHBOT=$AHBOT -DBUILD_METRICS=$METRICS -DBUILD_LOGIN_SERVER=$LOGIN_SERVER -DBUILD_GAME_SERVER=$GAME_SERVER -DBUILD_GIT_ID=$GIT_ID -DBUILD_RECASTDEMOMOD=$RECASTDEMOMOD -DBUILD_DOCS=$DOCS -DBUILD_WARNINGS=$WARNINGS -DBUILD_POSTGRESQL=$POSTGRESQL
+
 make clean
-make -j 8
+make -j $CORES
 make install
